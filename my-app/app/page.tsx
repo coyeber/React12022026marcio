905f84f865b40b319253ba8409a8df01c10e1a46
coyeber <sergiobjunior232@gmail.com>
2026-02-12 23:52:16 +0000

    Renoved

---
+++ my-app/app/page.tsx
@@ -0,0 +1,72 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-orange-500">
+      <h1 className="text-6xl font-bold text-white">Já é hora de viajar
+Não paro de me aventurar
+A emoção de batalha
+Assim eu vou me divertir
+Falem mal eu tô nem aí
+I can't get no satisfaction
+
+Desafios vem, pra me enfrentar
+Ninguém nunca vai me impedir de voar (let's fly high!)
+Todo o meu ki sinto explodir
+Vou lutar, vamos lá vencer!
+
+Não é possível nada me parar!
+Com meu poder de luta vai mostrar
+Que vou crescer, você vai ver (hey!)
+Só é preciso avançar
+Agora todos vão se assustar
+Qualquer limite posso ultrapassar
+Nada a temer, eu posso ver (hey!)
+É impossível me vencer
+Aqui é Dragon Ball Super
+Sr. Zen'Oh não pode me deter!
+
+Rumo a um novo espaço, eu vou!
+Obcecado pelo poder
+Que eu carrego em minhas mãos
+
+Sei que sou capaz de sorrir
+Em meio ao perigo e o caos
+Não me dou por satisfeito
+
+Você cairá
+Mas se acreditar
+Antes mesmo de tocar o chão você (vai voar!)
+
+Asas vão se abrir
+E vão te levar
+Para um novo céu, você vai ver!
+
+Você precisa derrubar
+As portas que vão se fechar
+E seu poder, vai se expandir (hey!)
+Como a fúria de um dragão!
+
+Barreiras não vão segurar
+A força que você vai despertar
+Seu próprio ser, se tornará (hey!)
+Um resplendor de aura azul!
+
+E a nossa busca continuará
+Com as super esferas do dragão
+
+Não é possível nada me parar!
+Com meu poder de luta vai mostrar
+Que vou crescer, você vai ver (hey!)
+Só é preciso avançar!
+
+Barreiras não vão segurar
+A força que você vai despertar
+Seu próprio ser, se tornará (hey!)
+Um resplendor de aura azul
+
+Ainda não é o fim! (Super!)
+Eu irei mais além!( Super!)
+Aqui é Dragon Ball Super
+Senhor Zen'oh, não pode me deter!</h1>
+    </div>
+  );
+}
